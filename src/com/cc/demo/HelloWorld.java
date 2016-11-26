20617aa738c36fcd58d83cc9f7687a391a4839e9
cc <cody_chen@163.com>
2016-11-27 02:05:39 +0800

    Initial commit

---
+++ src/com/cc/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cc.demo;
+
+/**
+ * Created by Administrator on 2016/11/27 0027.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
